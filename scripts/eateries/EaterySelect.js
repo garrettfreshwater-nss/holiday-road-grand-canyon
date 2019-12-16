b7754a19bfbc12c7d781ff3038ae1d207d5904ec
aborgra <audrey.borgra@gmail.com>
2019-12-16 15:57:20 -0600

    added nav bar select dropdowns

---
+++ scripts/eateries/EaterySelect.js
@@ -0,0 +1,43 @@
+import { useEateries } from "./EateryProvider.js"
+
+const eventHub = document.querySelector(".container")
+
+const contentElement = document.querySelector(".filters__eat")
+
+const EaterySelect = () => {
+  const eateries = useEateries()
+
+eventHub.addEventListener("change", changeEvent => {
+  if(changeEvent.target.id === "eaterySelect") {
+    const selectedEatery = changeEvent.target.value;
+    console.log(selectedEatery)
+
+    const message = new CustomEvent ("eaterySelected", {
+      detail: {
+        eatery: selectedEatery
+      }
+    })
+
+    eventHub.dispatchEvent(message)
+  }
+})
+
+
+
+
+const render = eateriesCollection => {
+  contentElement.innerHTML += `
+<select class="eateryDropdown" id="eaterySelect">
+<option value="0">Please select an eatery...</option>
+${eateriesCollection.map(
+eatery => `<option id="eaterySelect">${eatery}</option>`
+)}
+</select>
+`;
+};
+
+
+render(eateries)
+}
+
+export default EaterySelect
---
+++ scripts/parks/ParkSelect.js
@@ -0,0 +1,43 @@
+import { useParks } from "./ParkProvider.js"
+
+const eventHub = document.querySelector(".container")
+
+const contentElement = document.querySelector(".filters__parks")
+
+const ParkSelect = () => {
+  const parks = useParks()
+
+eventHub.addEventListener("change", changeEvent => {
+  if(changeEvent.target.id === "parkSelect") {
+    const selectedPark = changeEvent.target.value;
+    console.log(selectedPark)
+
+    const message = new CustomEvent ("parkSelected", {
+      detail: {
+        park: selectedPark
+      }
+    })
+
+    eventHub.dispatchEvent(message)
+  }
+})
+
+
+
+
+const render = parksCollection => {
+  contentElement.innerHTML += `
+<select class="parkDropdown" id="parkSelect">
+<option value="0">Please select a park...</option>
+${parksCollection.map(
+park => `<option id="parkSelect">${park}</option>`
+)}
+</select>
+`;
+};
+
+
+render(parks)
+}
+
+export default ParkSelect
