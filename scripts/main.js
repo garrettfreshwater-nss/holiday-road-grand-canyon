cef9b56548a4b5e0fa55bac8ec4d8765fc86a755
aborgra <audrey.borgra@gmail.com>
2019-12-16 16:07:09 -0600

    no major changes

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+import EaterySelect from "./eateries/EaterySelect.js";
+import ParkSelect from "./parks/ParkSelect.js";
+import AttractionSelect from "./attractions/AttractionSelect.js";
+
+EaterySelect()
+ParkSelect()
+AttractionSelect()
