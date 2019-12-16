5fdb76c0de37804097391d269906ac71123e1596
macgibbons <macgibbons@gmail.com>
2019-12-16 15:43:53 -0600

    added the dynamic html cards for attractions, parks, and eateries

---
+++ scripts/eateries/Eatery.js
@@ -0,0 +1,26 @@
+const eateryComponent = (eatery) => {
+    return `
+    <section class="eatery__card">
+    <div class="eatery__content">
+      <div class="eatery__name">${eatery.businessName}</div>
+      <img src="" alt="">
+    </div>
+  
+    <dialog class="dialog__eatery" id="details__${eatery.id}">
+      <div class="eatery__name">${eatery.businessName}</div>
+      <div class="eatery__city">${eatery.city}</div>
+      <div class="eatery__state">${eatery.state}</div>
+      <div class="eatery__description">${eatery.description}</div>
+      <div class="eatery__ameneties">${eatery.ameneties}</div>
+      <div class="eatery__souvenirs">${eatery.souvenirs}</div>
+      <div class="eatery__restrooms">${eatery.restrooms}</div>
+      
+      <button class="close__dialog">Hide</button>
+    </dialog>
+  </section>
+    `
+}
+
+
+
+export default eateryComponent
